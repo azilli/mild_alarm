571486f8644d13383a288fda6cd70067f68e8c05
azilli <asanosora@gmail.com>
2016-03-18 14:47:38 -0700

    changes to server code

--- webserver1.py
+++ webserver1.py
@@ -8,16 +8,14 @@ listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print 'Serving HTTP on port %s ...' % PORT
 
+f = open('alarm1.html', 'r')
+html_page = f.read()
+f.close()
+
 while True:
   client_connection, client_address = listen_socket.accept()
   request = client_connection.recv(1024)
   print request
-
-  http_response = """\
-HTTP/1.1 200 OK
-
-Hello, World!
-"""
-
+  http_response = 'HTTP/1.1 200 OK\n\n' + html_page
   client_connection.sendall(http_response)
   client_connection.close()
